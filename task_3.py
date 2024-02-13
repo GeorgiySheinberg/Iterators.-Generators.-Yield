dd2d014c6ccdeff7dc21e9f27d5713c4641fc6f3
Георгий Шейнберг <georgiysheinberg@gmail.com>
2024-02-13 19:38:17 +0300

    third commit +3 task

---
+++ task_3.py
@@ -0,0 +1,44 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.stack = [iter(list_of_list)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self.stack) == 0:
+            raise StopIteration
+        try:
+            item = next(self.stack[-1])
+            if isinstance(item, list):
+                self.stack.append(iter(item))
+                return self.__next__()
+            else:
+                return item
+        except StopIteration:
+            try:
+                self.stack.pop()
+            except IndexError:
+                raise StopIteration
+            return self.__next__()
+
+
+def test_3():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_3()
